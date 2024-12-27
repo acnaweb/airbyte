6f50570af960919a6c3db7fdbfaf4bd6e4678479
AC <ac@marketmining.com.br>
2024-12-27 16:20:53 -0300

    Update docs

--- scripts/load_source_cdc.sql
+++ scripts/load_source_cdc.sql
@@ -42,3 +42,57 @@ CREATE PUBLICATION pub1 FOR TABLE cursos;
 INSERT INTO cursos VALUES(1006, 'FIAMED');
 DELETE FROM cursos WHERE NAME = 'FEI';
 
+-- Cria a tabela na fonte
+CREATE TABLE tb_cliente(
+  id integer PRIMARY KEY,
+  nome VARCHAR(200),
+  updated_at timestamp DEFAULT NOW() NOT NULL
+);
+
+-- Insere alguns registros
+INSERT INTO tb_cliente(id, nome) VALUES(1, 'Bob');
+INSERT INTO tb_cliente(id, nome) VALUES(2, 'Maria');
+INSERT INTO tb_cliente(id, nome) VALUES(3, 'Joana');
+
+-- Verifica os registros
+SELECT * FROM tb_cliente;
+
+-- Cria a função
+CREATE OR REPLACE FUNCTION dsa_fn_set_timestamp()
+RETURNS TRIGGER AS '
+ BEGIN
+   NEW.updated_at = NOW();
+   RETURN NEW;
+ END;
+'
+LANGUAGE plpgsql;
+
+-- Cria a trigger
+CREATE TRIGGER dsa_tr_set_timestamp
+  BEFORE UPDATE ON tb_cliente
+  FOR EACH ROW
+  EXECUTE PROCEDURE dsa_fn_set_timestamp();
+
+SELECT * FROM tb_cliente;
+
+UPDATE tb_cliente 
+SET nome = 'Carolina' 
+WHERE id = 2;
+
+SELECT * FROM tb_cliente;
+
+UPDATE tb_cliente 
+SET nome = 'Fernando' 
+WHERE id = 1;
+
+SELECT * FROM tb_cliente;
+
+UPDATE tb_cliente 
+SET nome = 'Ana' 
+WHERE id = 3;
+
+SELECT * FROM tb_cliente;
+
+INSERT INTO tb_cliente(id, nome) VALUES(4, 'Zico');
+
+SELECT * FROM tb_cliente;
